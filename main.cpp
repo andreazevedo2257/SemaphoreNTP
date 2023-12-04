80f492386b016fc10d4d3c6916317259aeb1e9e4
AndBarata <146003937+AndBarata@users.noreply.github.com>
2023-12-04 16:45:00 +0000

    Working lib glfw with cmake

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World V2!" << std::endl;
+
+    GLFWwindow* window;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
     return 0;
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+Build and configure the project by typing the following comands on the terminal
+
+    ./configure.sh
+    ./build.sh
+    ./run
